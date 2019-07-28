c1f090b1d4c47352a91e4049d5445dd87340b9a5
Neg999 <enegocomley@gmail.com>
2019-07-28 18:48:23 +0100

    Added the project.
    Has button images.

---
+++ Teach-Fast Type-Fast/Assets/Scripts/ManagerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerController : MonoBehaviour {
+    public ManagerController managerScript;
+    public GameObject manager;
+	// Use this for initialization
+	void Awake () {
+        if (managerScript == null)
+        {
+            managerScript = this;
+            manager = gameObject;
+            DontDestroyOnLoad(manager);
+        }
+        //That makes sure there is only one Manager and makes sure the manager always exists.
+	}
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
