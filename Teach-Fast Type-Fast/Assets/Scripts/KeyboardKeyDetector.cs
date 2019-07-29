eb949cf5d048fc8dc0fd861040e54838f8001975
Neg999 <enegocomley@gmail.com>
2019-07-29 14:35:18 +0100

    Created a Keyboard
    The Keboard detects that you have pressd a key and keys will then change colour and size to sow that it has been pressed.

---
+++ Teach-Fast Type-Fast/Assets/Scripts/KeyboardKeyDetector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardKeyDetector : MonoBehaviour
+{
+    const float scaleDownButton = 0.8f;
+    const float scaleUpButton = 1.0f;
+
+    public string keyName;
+    Text text;
+    Transform imageTransform;
+    Transform textTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        var parent = GameObject.Find(text.text);
+        if(parent != null)
+        {
+            imageTransform = parent.transform;
+        }
+        textTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.color = Color.white;
+        imageTransform.localScale = new Vector3(scaleUpButton, scaleUpButton);
+        textTransform.localScale = new Vector3(2, 2);
+        if (Input.GetKey(keyName))
+        {
+            text.color = Color.red;
+            Debug.Log(text.text);
+            imageTransform.localScale = new Vector3(scaleDownButton, scaleDownButton);
+            textTransform.localScale = new Vector3(2.5f, 2.5f);
+            
+        }
+    }
+}
