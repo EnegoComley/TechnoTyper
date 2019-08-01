f41b6a6d646df0fd17210655a2eb114ff5422014
Neg999 <enegocomley@gmail.com>
2019-08-01 13:59:21 +0100

    Added stuff to the test screen.
    I have Highlighted text to show which key to type. Also added a WPM and Acurracy counter and text box.

--- Teach-Fast Type-Fast/Assets/Scripts/TypingTextScript.cs
+++ Teach-Fast Type-Fast/Assets/Scripts/TypingTextScript.cs
@@ -2,24 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TypingTextScript : MonoBehaviour
 {
-    public Text thisTextsTextComponent;
-    string theActualText;
+    public TextMeshProUGUI thisTextsTextComponent;
+    public TextMeshProUGUI statsBar;
+    public string theActualText;
     int character;
     string currentLetter;
     string[] letters = { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "z", "x", "c", "v", "b", "n", "m", ",", "." };
-    public float acuracy;
     float numberOfMistakes;
     float theActualTextLength;
-    public float wPM;
+    int keysPressed;
     // Start is called before the first frame update
     void Start()
     {
         theActualText = thisTextsTextComponent.text;
         currentLetter = theActualText[character].ToString();
         theActualTextLength = theActualText.Length;
+        AddMark();
+        UnHighlight();
     }
 
     // Update is called once per frame
@@ -30,7 +33,8 @@ public class TypingTextScript : MonoBehaviour
             if (currentLetter.ToString() == " ")
             {
                 currentLetter = "space";
-            } 
+            }
+
             if (Input.GetKey(currentLetter.ToString()))
             {
                 PressedTheRightKey();
@@ -39,17 +43,27 @@ public class TypingTextScript : MonoBehaviour
             {
                 PressedTheWrongKey();
             }
-            
+
         }
+
+        statsBar.text = $"You are currently typing at {wPM()} Words Per Minute. Your current accuracy is {acuracy()}%"  ;
+
     }
 
     public void PressedTheRightKey()
     {
-        theActualText = theActualText.Insert(character, "<color=green>");
-        character = character + 14;
-        theActualText = theActualText.Insert(character, "</color>");
-        character = character + 8;
+        if (currentLetter == "space")
+        {
+            theActualText = theActualText.Remove(character, 1);
+            theActualText = theActualText.Insert(character, "_");
+        }
+        theActualText = theActualText.Replace("<mark=#9D9D9D00>", "<color=green>");
+        theActualText = theActualText.Replace("<mark=#9D9D9D40>", "<color=green>");
+        theActualText = theActualText.Replace("</mark>","</color>");
+        character = character + 6;
+        AddMark();
         thisTextsTextComponent.text = theActualText;
+        keysPressed++;
         if (theActualText.Length > character)
         {
             currentLetter = theActualText[character].ToString();
@@ -59,44 +73,114 @@ public class TypingTextScript : MonoBehaviour
             Finish();
         }
 
-        
+
     }
 
     public void PressedTheWrongKey()
     {
-        theActualText = theActualText.Insert(character, "<color=red>");
-        character = character + 11;
+
         for (int i = 0; letters.Length > i; i++)
         {
             if (Input.GetKeyDown(letters[i].ToString()))
             {
                 theActualText = theActualText.Remove(character, 1);
                 theActualText = theActualText.Insert(character, letters[i].ToString());
-                character++;
-                
+                theActualText = theActualText.Replace("<mark=#9D9D9D00>", "<color=red>");
+                theActualText = theActualText.Replace("<mark=#9D9D9D40>", "<color=red>");
+                theActualText = theActualText.Replace("</mark>", "</color>");
+                character = character + 4;
+                Debug.Log(theActualText[character]);
+                AddMark();
+                thisTextsTextComponent.text = theActualText;
+                numberOfMistakes++;
+                keysPressed++;
+                if (theActualText.Length > character)
+                {
+                    currentLetter = theActualText[character].ToString();
+                }
+                else
+                {
+                    Finish();
+                }
+
             }
         }
-        theActualText = theActualText.Insert(character, "</color>");
-        character = character + 8;
-        thisTextsTextComponent.text = theActualText;
-        numberOfMistakes++;
-       
-        if (theActualText.Length > character)
+        if (Input.GetKeyDown("space"))
         {
-            currentLetter = theActualText[character].ToString();
-        }
-        else
-        {
-            Finish();
+            theActualText = theActualText.Remove(character, 1);
+            theActualText = theActualText.Insert(character, "_");
+            theActualText = theActualText.Replace("<mark=#9D9D9D00>", "<color=red>");
+            theActualText = theActualText.Replace("<mark=#9D9D9D40>", "<color=red>");
+            theActualText = theActualText.Replace("</mark>", "</color>");
+            character = character + 4;
+            Debug.Log(theActualText[character]);
+            AddMark();
+            thisTextsTextComponent.text = theActualText;
+            numberOfMistakes++;
+            keysPressed++;
+            if (theActualText.Length > character)
+            {
+                currentLetter = theActualText[character].ToString();
+            }
+            else
+            {
+                Finish();
+            }
         }
 
+
+
     }
-    
+
     public void Finish()
     {
-        acuracy = 100*((theActualTextLength - numberOfMistakes) / theActualTextLength);
-        Debug.Log(acuracy + "%");
-        wPM = ((theActualTextLength/5)/(Time.time/ 60));
-        Debug.Log(wPM + " Words per Minute");
+        Debug.Log(acuracy() + "%");
+        Debug.Log(wPM() + " Words per Minute");
     }
+
+    void Highlight()
+    {
+        theActualText = theActualText.Replace("<mark=#9D9D9D00>", "<mark=#9D9D9D40>");
+        thisTextsTextComponent.text = theActualText;
+        Invoke("UnHighlight", 0.2f);
+    }
+
+    void UnHighlight()
+    {
+        theActualText = theActualText.Replace("<mark=#9D9D9D40>","<mark=#9D9D9D00>");
+        thisTextsTextComponent.text = theActualText;
+        Invoke("Highlight",0.2f);
+    }
+
+    void AddMark()
+    {
+        Debug.Log(theActualText[character]);
+        theActualText = theActualText.Insert(character, "<mark=#9D9D9D40>");
+        character = character + 16;
+        theActualText = theActualText.Insert(character + 1, "</mark>");
+        thisTextsTextComponent.text = theActualText;
+    }
+    public float thing;
+
+    public int wPM()
+    {
+        return (int)((keysPressed / 5) / (Time.time / 60));
+    }
+
+    public int acuracy()
+    {
+        if (keysPressed == 0)
+        {
+            return (100);
+        }
+        return (int)(100 * ((keysPressed - numberOfMistakes) / keysPressed));
+    }
+
+    public void SetLevel(int level)
+    {
+
+    }
+
+
 }
+
